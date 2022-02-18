b1439b3ed5194cbcc78e5a975839a2563a654146
Benjamin <spotswoodb@gmail.com>
2022-02-17 20:53:08 -0600

    application running properly in dev

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+import os
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
-from send_mail import send_mail
+import pandas as pd
+# from send_mail import send_mail
+
+a = pd.read_csv("Exit_Velocity_Percentiles_2021_LD_FB_EV_Percentiles_Table.csv")
+a.to_html("EVP_2021_LD_FB_EVP_Table.html")
+html_file = a.to_html()
+
+load_dotenv()
+POSTGRES_URI = os.environ.get('POSTGRES_URI')
+
+
 
 app = Flask(__name__)
 
@@ -8,7 +20,7 @@ ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_URI
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ''
@@ -21,17 +33,15 @@ db = SQLAlchemy(app)
 class Feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
-    rating = db.Column(db.Integer)
-    comments = db.Column(db.Text())
+    player_id = db.Column(db.String(200), unique=True)
+    player = db.Column(db.String(200), unique=True)
+    
 
-    def __init__(self, customer, dealer, rating, comments):
-        self.customer = customer
-        self.dealer = dealer
-        self.rating = rating
-        self.comments = comments
+    def __init__(self, player_id, player):
+        self.player_id = player_id
+        self.player = player
 
+# class ExitVelocityPercentiles2021LDFBEVPercentilesTable(html_file):
 
 @app.route('/')
 def index():
@@ -41,18 +51,16 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        customer = request.form['customer']
-        dealer = request.form['dealer']
-        rating = request.form['rating']
-        comments = request.form['comments']
+        player_id = request.form['player_id']
+        player = request.form['player']
         # print(customer, dealer, rating, comments)
-        if customer == '' or dealer == '':
+        if player == '' or player_id == '':
             return render_template('index.html', message='Please enter required fields')
-        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-            data = Feedback(customer, dealer, rating, comments)
+        if db.session.query(Feedback).filter(Feedback.player_id == player_id).count() == 0:
+            data = Feedback(player_id, player)
             db.session.add(data)
             db.session.commit()
-            send_mail(customer, dealer, rating, comments)
+            # send_mail(customer, dealer, rating, comments)
             return render_template('success.html')
         return render_template('index.html', message='You have already submitted feedback')
 
