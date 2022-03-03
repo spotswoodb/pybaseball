aa69c87d8980077099969ab0c3ef0758a42cdb5a
Benjamin <spotswoodb@gmail.com>
2022-03-02 21:08:29 -0600

    adding csv reading to index route

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 
+# file = pd.read_csv("stats.csv")
+# file.to_html('FirstStatcastTable.html')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    file = pd.read_csv("stats.csv")
+    file.to_html("FirstStatcastTable.html")
+    return render_template("FirstStatcastTable.html")
