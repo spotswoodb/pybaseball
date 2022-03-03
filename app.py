56b5df8abadadaeb3aec7cb4affa316ecee3bf88
Benjamin <spotswoodb@gmail.com>
2022-03-03 16:47:05 -0600

    finish model, create player table in database

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from click import launch
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
@@ -35,8 +36,33 @@ class Player(db.Model):
     player_id = db.Column(db.Integer, unique=True)
     year = db.Column(db.Integer)
     player_age = db.Column(db.Integer)
-    xba = db.Column
+    xba = db.Column(db.Integer)
+    xslg = db.Column(db.Integer)
+    woba = db.Column(db.Integer)
+    xwoba = db.Column(db.Integer)
+    xobp = db.Column(db.Integer)
+    xiso = db.Column(db.Integer)
+    exit_velocity_avg = db.Column(db.Integer)
+    launch_angle_avg = db.Column(db.Integer)
+    sweet_spot_percent = db.Column(db.Integer)
+    barrel_batted_rate = db.Column(db.Integer)
 
+    def __init__(self, last_name, first_name, player_id, year, player_age, xba, xslg, woba, xwoba, xobp, xiso, exit_velocity_avg, launch_angle_avg, sweet_spot_percent, barrel_batted_rate):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.player_id = player_id
+        self.year = year
+        self.player_age = player_age
+        self.xba = xba
+        self.xslg = xslg
+        self.woba = woba
+        self.xwoba = xwoba
+        self.xobp = xobp
+        self.xiso = xiso
+        self.exit_velocity_avg = exit_velocity_avg
+        self.launch_angle_avg = launch_angle_avg
+        self.sweet_spot_percent = sweet_spot_percent
+        self.barrel_batted_rate = barrel_batted_rate
 
 
 @app.route('/')
